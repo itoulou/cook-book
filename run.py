3c90b0c94dad91bd91e3c367ccaa72fb4a48006e
Ivan Toulou <touloumbadjiani@gmail.com>
2018-09-28 20:06:37 +0000

    Created for loops around the selected options in both addrecipe and editrecipe forms. created update_recipe function to update recipe and return to viewrecipe.html

--- run.py
+++ run.py
@@ -15,13 +15,23 @@ def return_home():
     
 @app.route('/add_recipe')
 def to_add_recipe_page():
-    return render_template('addrecipe.html')
+    serves = ["1", "2", "3", "4", "5", "6+"]
+    cuisine = [
+               "African", "American", "British", "Carribean", "Chinese",
+               "East European", "French", "Greek", "Indian", "Irish",
+               "Italian", "Japanese", "Korean", "Mexican",
+               "Middle Eastern and Persian", "Nordic", "Spanish", "Thai",
+               "Other"
+                ]
+    recipes = mongo.db.recipes.find()            
+    return render_template('addrecipe.html', serves=serves, cuisine=cuisine,
+                            recipes=recipes)
     
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
     recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
-    return redirect(url_for('return_home'))
+    return redirect(url_for('all_recipes'))
 
 @app.route('/all_recipes')
 def all_recipes():
@@ -33,9 +43,40 @@ def edit_recipe(recipe_id):
     recipe_selected = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
     return render_template('viewrecipe.html', recipe_selected=recipe_selected)
 
-@app.route('/edit_recipe')
-def to_edit_recipe():
-    render_template('editrecipe.html')
+@app.route('/edit_recipe/<recipe_id>')
+def to_edit_recipe(recipe_id):
+    serves = ["1", "2", "3", "4", "5", "6+"]
+    cuisine = [
+               "African", "American", "British", "Carribean", "Chinese",
+               "East European", "French", "Greek", "Indian", "Irish",
+               "Italian", "Japanese", "Korean", "Mexican",
+               "Middle Eastern and Persian", "Nordic", "Spanish", "Thai",
+               "Other"
+                ]
+    recipe_selected =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    return render_template('editrecipe.html', recipe=recipe_selected, serves=serves, cuisine=cuisine)
+
+@app.route('/update_recipe/<recipe_id>', methods=["POST"])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+    recipes.update( {"_id": ObjectId(recipe_id)},
+    
+    {
+        "username": request.form.get('username'),
+        "name_of_dish": request.form.get('name_of_dish'),
+        "serves": request.form.get('serves'),
+        "cuisine": request.form.get('cuisine'),
+        "ingredients": request.form.get('ingredients'),
+        "method": request.form.get('method'),
+        "contain_nuts": request.form.get('contain_nuts'),
+        "contain_gluten": request.form.get('contain_gluten'),
+        "contain_lactose": request.form.get('contain_lactose'),
+        "batch_cook": request.form.get('batch_cook'),
+        "image_url": request.form.get('image_url')
+        
+        
+    })
+    return redirect(url_for('all_recipes'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
