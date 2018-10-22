611c727ef35dedfc76a0a22dbe849f59aae1cfad
Ivan Toulou <touloumbadjiani@gmail.com>
2018-10-22 11:36:08 +0000

    all checkboxes send true or false when selected or unselected when new recipe added or recipe updated

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import operator
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -30,8 +31,37 @@ def to_add_recipe_page():
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
     recipes = mongo.db.recipes
-    recipes.insert_one(request.form.to_dict())
+    recipe_dict = sanitize_form_dictionary(request.form.to_dict())
+
+    recipes.insert_one(recipe_dict)
     return redirect(url_for('all_recipes'))
+    
+def sanitize_form_dictionary(recipe_dict):
+    try:
+        recipe_dict['contain_gluten']=recipe_dict['contain_gluten']=='on'
+    
+    except:
+        recipe_dict['contain_gluten']=False
+    
+    try:
+        recipe_dict['contain_lactose']=recipe_dict['contain_lactose']=='on'
+    
+    except:
+        recipe_dict['contain_lactose']=False    
+    
+    try:
+        recipe_dict['contain_nuts']=recipe_dict['contain_nuts']=='on'
+    
+    except:
+        recipe_dict['contain_nuts']=False    
+    
+    try:
+        recipe_dict['batch_cook']=recipe_dict['batch_cook']=='on'
+    
+    except:
+        recipe_dict['batch_cook']=False
+        
+    return recipe_dict
 
 @app.route('/all_recipes')
 def all_recipes():
@@ -59,24 +89,65 @@ def to_edit_recipe(recipe_id):
 @app.route('/update_recipe/<recipe_id>', methods=["POST"])
 def update_recipe(recipe_id):
     recipes = mongo.db.recipes
+    recipe_dict = sanitize_form_dictionary(request.form.to_dict())
+    
     recipes.update( {"_id": ObjectId(recipe_id)},
     
     {
-        "username": request.form.get('username'),
-        "name_of_dish": request.form.get('name_of_dish'),
-        "serves": request.form.get('serves'),
-        "cuisine": request.form.get('cuisine'),
-        "ingredients": request.form.get('ingredients'),
-        "method": request.form.get('method'),
-        "contain_nuts": request.form.get('contain_nuts'),
-        "contain_gluten": request.form.get('contain_gluten'),
-        "contain_lactose": request.form.get('contain_lactose'),
-        "batch_cook": request.form.get('batch_cook'),
-        "image_url": request.form.get('image_url')
+        "username": recipe_dict['username'],
+        "name_of_dish": recipe_dict['name_of_dish'],
+        "serves": recipe_dict['serves'],
+        "cuisine": recipe_dict['cuisine'],
+        "ingredients": recipe_dict['ingredients'],
+        "method": recipe_dict['method'],
+        "contain_nuts": recipe_dict['contain_nuts'],
+        "contain_gluten": recipe_dict['contain_gluten'],
+        "contain_lactose": recipe_dict['contain_lactose'],
+        "batch_cook": recipe_dict['batch_cook'],
+        "image_url": recipe_dict['image_url']
         
         
     })
+    
     return redirect(url_for('all_recipes'))
+    
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
+    return redirect(url_for('all_recipes'))
+
+@app.route('/gluten_free')
+def sort_gluten_free():
+  
+    gluten_free_recipes = mongo.db.recipes.find(
+                                        {'contain_gluten': {"$exists": False}})
+                                        
+    return render_template('glutenfree.html', gf=gluten_free_recipes)
+    
+@app.route('/nut_free')
+def sort_nut_free():
+  
+    nut_free_recipes = mongo.db.recipes.find(
+                                        {'contain_nuts': {"$exists": False}})
+                                        
+    return render_template('nutfree.html', nf=nut_free_recipes)    
+    
+@app.route('/lactose_free')
+def sort_lactose_free():
+  
+    lactose_free_recipes = mongo.db.recipes.find(
+                                        {'contain_lactose': {"$exists": False}})
+                                        
+    return render_template('lactosefree.html', lf=lactose_free_recipes)
+
+@app.route('/batch_cook')
+def sort_batch_cook():
+  
+    batch_cooks = mongo.db.recipes.find(
+                                        {'batch_cook': {"$exists": True}})
+                                        
+    return render_template('batchcook.html', batch_cooks=batch_cooks)     
+    
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
